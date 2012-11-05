b6289327be57ced53036daa2ca95b77b2a0a0c17
Eric Zounes <zounes99@gmail.com>
2012-11-04 17:00:14 -0800

    Lab4.

---
+++ Lab4_zounese.m
@@ -0,0 +1,41 @@
+% Name: Eric Zounes
+% Date: 11/04/12
+% Lab 4
+ 
+% 1. 
+N = 10;
+x = linspace(-5,5,N+1);
+f = inline('1./(1+x.*x)', 'x');
+y = f(x)
+plot(x,y,'o')
+title('N+1 = 11 equally-spaced data points')
+t = [-5:.1:5];
+figure;
+plot(t,f(t),'-')
+title('Runge function')
+
+% 2. 
+
+PN = polyfit(x,y,N);
+v = polyval(PN,t);
+err = norm(f(t)-v,inf) 
+
+figure; 
+plot(x,y,'o',t,f(t),'-',t,v,'--')
+title(sprintf('f(t) and P_{10}(t), err%g', err))
+
+
+% 3. Interpolation at Chebychev nodes: 
+
+K = N+1;
+a = -5;
+b = 5;
+xcheb = zeros(1,K);
+for i = 1:K
+xcheb(i) = (a+b)/2 + (b-a)/2 * cos( (i-.5)*pi/K);
+end
+ycheb = f(xcheb);
+
+% 4. Repeat 1, 2, and 3 with N = 20 and N = 50. Explain what behavior you observe. 
+
+
